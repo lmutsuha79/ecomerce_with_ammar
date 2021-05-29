6f3e7be2cba5da6c38bd7a6f09bfad319f1b4863
3mora2 <Ammaralkotb.2020@azhar.edu.eg>
2021-05-29 13:19:27 +0200

    Update __init__.py

--- market/__init__.py
+++ market/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-
+from flask_wtf import CSRFProtect
 
 SECRET_KEY = b'9\x95\x83\x97\xb0;\xa1\xb2k\xae6\x0e\x00\xb8\xf4\x8a\x03*\xcf\xb6\xcc\x94 L\xbe\xef\xbc\x85\x96ol\xb5'
 
@@ -14,6 +14,7 @@ app.config['SECRET_KEY'] = SECRET_KEY
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+csrf = CSRFProtect(app)
 login_manager = LoginManager(app=app)
 login_manager.login_view = '/login'
 
