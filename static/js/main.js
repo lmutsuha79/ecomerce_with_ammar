09a767833132222dadc4b8b8a1cec464f52852e8
Khelil Yasser Djamel Eddie <lmutsuha79@gmail.com>
2021-06-23 19:15:58 +0100

    add animation to nav btn bolets when clicking

--- static/js/main.js
+++ static/js/main.js
@@ -6,13 +6,19 @@ let main_nav_of_links = document.getElementById('main_nav_of_links');
 
 
 
+
 function change_nav_style() {
     main_nav_of_links.classList.toggle('open_mb_nav');
 
 
 
 }
-mb_nav_btn.onclick = change_nav_style;
+mb_nav_btn.onclick = function () {
+    change_nav_style();
+    // change nav btn bolets style
+    mb_nav_btn.classList.toggle('mb_nav_open_style');
+
+}
 
 // ############################################
 // slider js 
