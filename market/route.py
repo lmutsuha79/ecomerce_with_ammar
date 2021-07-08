43807c6f9a087ecf8f8813a1946418dc8b4f9c55
3mora2 <Ammaralkotb.2020@azhar.edu.eg>
2021-07-08 16:03:51 +0200

    trend page

--- market/route.py
+++ market/route.py
@@ -3,6 +3,7 @@ from flask import render_template, request, redirect, url_for, jsonify
 from .form import RegistrationForm, LoginForm
 from .model import db, User
 from flask_login import login_user, current_user, logout_user
+import requests
 
 
 @login_manager.user_loader
@@ -80,7 +81,6 @@ def registration_page():
 
 @app.route('/check-reg', methods=['POST'])
 def check_user_mail():
-
     field_ = request.form.get('username')
     if field_:
         if User.query.filter_by(username=field_).first():
@@ -111,3 +111,52 @@ def page_not_found(e):
 @app.route('/games')
 def games():
     return render_template('games.html')
+
+
+@app.route('/trend')
+def trend_page():
+    headers = {
+        "accept": "application/json, text/plain, */*",
+        "accept-language": "ar,en-US;q=0.9,en;q=0.8",
+        "content-type": "application/json;charset=UTF-8",
+        "sec-ch-ua": "\" Not;A Brand\";v=\"99\", \"Google Chrome\";v=\"91\", \"Chromium\";v=\"91\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "x-requested-with": "XMLHttpRequest"
+    }
+    body = "{\"query\":\"query collectionLayoutQuery($locale: String, $country: String!, $slug: String) " \
+           "{\\n  Storefront {\\n    collectionLayout(locale: $locale, slug: $slug) {\\n      _activeDate\\n      _locale\\n      _metaTags\\n      _slug\\n      _title\\n      _urlPattern\\n      lastModified\\n      regionBlock\\n      affiliateId\\n      takeover {\\n        banner {\\n          altText\\n          src\\n        }\\n        description\\n        eyebrow\\n        title\\n      }\\n      seo {\\n        title\\n        description\\n        keywords\\n        image {\\n          src\\n          altText\\n        }\\n        twitter {\\n          title\\n          description\\n        }\\n        og {\\n          title\\n          description\\n          image {\\n            src\\n            alt\\n          }\\n        }\\n      }\\n      collectionOffers {\\n        title\\n        id\\n        namespace\\n        description\\n        effectiveDate\\n        keyImages {\\n          type\\n          url\\n        }\\n        seller {\\n          id\\n          name\\n        }\\n        releaseDate\\n        prePurchase\\n        productSlug\\n        urlSlug\\n        url\\n        items {\\n          id\\n          namespace\\n        }\\n        customAttributes {\\n          key\\n          value\\n        }\\n        categories {\\n          path\\n        }\\n        linkedOfferId\\n        linkedOffer {\\n          effectiveDate\\n          customAttributes {\\n            key\\n            value\\n          }\\n        }\\n        catalogNs {\\n          mappings(pageType: \\\"productHome\\\") {\\n            pageSlug\\n            pageType\\n          }\\n        }\\n        offerMappings {\\n          pageSlug\\n          pageType\\n        }\\n        price(country: $country) {\\n          totalPrice {\\n            currencyCode\\n            currencyInfo {\\n              decimals\\n              symbol\\n            }\\n            discountPrice\\n            originalPrice\\n            voucherDiscount\\n            discount\\n            fmtPrice(locale: $locale) {\\n              originalPrice\\n              discountPrice\\n              intermediatePrice\\n            }\\n          }\\n          lineOffers {\\n            appliedRules {\\n              id\\n              endDate\\n            }\\n          }\\n        }\\n      }\\n      pageTheme {\\n        preferredMode\\n        light {\\n          theme\\n          accent\\n        }\\n        dark {\\n          theme\\n          accent\\n        }\\n      }\\n      redirect {\\n        code\\n        url\\n      }\\n    }\\n  }\\n}\\n\"," \
+           "\"variables\":{\"locale\":\"ar\",\"slug\":\"top-sellers\",\"country\":\"EG\"}}"
+    r = requests.post("https://www.epicgames.com/graphql", headers=headers, data=body)
+    if r.ok:
+        data = r.json()
+        data = data.get('data',).get('Storefront',).get('collectionLayout').get('collectionOffers')
+    else:
+        data = {}
+    return render_template('trend.html', data=data)
+
+
+'''
+fetch("https://www.epicgames.com/graphql", {
+  "headers": {
+    "accept": "application/json, text/plain, */*",
+    "accept-language": "ar,en-US;q=0.9,en;q=0.8",
+    "content-type": "application/json;charset=UTF-8",
+    "sec-ch-ua": "\" Not;A Brand\";v=\"99\", \"Google Chrome\";v=\"91\", \"Chromium\";v=\"91\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "x-requested-with": "XMLHttpRequest"
+  },
+  "referrer": "https://www.epicgames.com/store/ar/collection/top-sellers",
+  "referrerPolicy": "no-referrer-when-downgrade",
+  "body": "{\"query\":\"query collectionLayoutQuery($locale: String, $country: String!, $slug: String) {\\n  Storefront {\\n    collectionLayout(locale: $locale, slug: $slug) {\\n      _activeDate\\n      _locale\\n      _metaTags\\n      _slug\\n      _title\\n      _urlPattern\\n      lastModified\\n      regionBlock\\n      affiliateId\\n      takeover {\\n        banner {\\n          altText\\n          src\\n        }\\n        description\\n        eyebrow\\n        title\\n      }\\n      seo {\\n        title\\n        description\\n        keywords\\n        image {\\n          src\\n          altText\\n        }\\n        twitter {\\n          title\\n          description\\n        }\\n        og {\\n          title\\n          description\\n          image {\\n            src\\n            alt\\n          }\\n        }\\n      }\\n      collectionOffers {\\n        title\\n        id\\n        namespace\\n        description\\n        effectiveDate\\n        keyImages {\\n          type\\n          url\\n        }\\n        seller {\\n          id\\n          name\\n        }\\n        releaseDate\\n        prePurchase\\n        productSlug\\n        urlSlug\\n        url\\n        items {\\n          id\\n          namespace\\n        }\\n        customAttributes {\\n          key\\n          value\\n        }\\n        categories {\\n          path\\n        }\\n        linkedOfferId\\n        linkedOffer {\\n          effectiveDate\\n          customAttributes {\\n            key\\n            value\\n          }\\n        }\\n        catalogNs {\\n          mappings(pageType: \\\"productHome\\\") {\\n            pageSlug\\n            pageType\\n          }\\n        }\\n        offerMappings {\\n          pageSlug\\n          pageType\\n        }\\n        price(country: $country) {\\n          totalPrice {\\n            currencyCode\\n            currencyInfo {\\n              decimals\\n              symbol\\n            }\\n            discountPrice\\n            originalPrice\\n            voucherDiscount\\n            discount\\n            fmtPrice(locale: $locale) {\\n              originalPrice\\n              discountPrice\\n              intermediatePrice\\n            }\\n          }\\n          lineOffers {\\n            appliedRules {\\n              id\\n              endDate\\n            }\\n          }\\n        }\\n      }\\n      pageTheme {\\n        preferredMode\\n        light {\\n          theme\\n          accent\\n        }\\n        dark {\\n          theme\\n          accent\\n        }\\n      }\\n      redirect {\\n        code\\n        url\\n      }\\n    }\\n  }\\n}\\n\",\"variables\":{\"locale\":\"ar\",\"slug\":\"top-sellers\",\"country\":\"EG\"}}",
+  "method": "POST",
+  "mode": "cors",
+  "credentials": "include"
+});
+
+'''
